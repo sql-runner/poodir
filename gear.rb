d795aa9353633d0d6ae8ae76435b7a5e33be98f3
a2k11 <adam.kaewell@gmail.com>
2014-12-13 16:28:57 -0500

    Isolate Multiparameter Initialization, avoids having multiple dependencies on the order of those arguments

--- gear.rb
+++ gear.rb
@@ -1,28 +1,34 @@
 require "./wheel"
 
-class Gear
-  attr_reader :chainring, :cog, :wheel
-  def initialize(args)
-    args = defaults.merge(args)
-    @chainring = args[:chainring]
-    @cog = args[:cog]
-    @wheel = args[:wheel]
-  end
+module SomeFramework
+  class Gear
+    attr_reader :chainring, :cog, :wheel
+    def initialize(chainring, cog, wheel)
+      @chainring = chainring
+      @cog = cog
+      @wheel = wheel
+    end
 
-  def ratio
-    chainring / cog.to_f
-  end
+    def ratio
+      chainring / cog.to_f
+    end
 
-  def gear_inches
-    ratio * wheel.diameter
-  end
-
-  def defaults
-    { chainring: 40, cog: 18 }
+    def gear_inches
+      ratio * wheel.diameter
+    end
   end
 end
 
-puts Gear.new(
+module GearWrapper
+  def self.gear(args)
+    SomeFramework::Gear.new(
+      args[:chainring],
+      args[:cog],
+      args[:wheel])
+  end
+end
+
+puts GearWrapper.gear(
   chainring: 52,
   cog: 11,
   wheel: Wheel.new(26, 1.5)).gear_inches
