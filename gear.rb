8e2d81daad4498228574ed8acfe87867724f87f8
a2k11 <adam.kaewell@gmail.com>
2014-12-13 16:42:46 -0500

    Reversing Dependencies

--- gear.rb
+++ gear.rb
@@ -1,34 +1,21 @@
 require "./wheel"
 
-module SomeFramework
-  class Gear
-    attr_reader :chainring, :cog, :wheel
-    def initialize(chainring, cog, wheel)
-      @chainring = chainring
-      @cog = cog
-      @wheel = wheel
-    end
-
-    def ratio
-      chainring / cog.to_f
-    end
-
-    def gear_inches
-      ratio * wheel.diameter
-    end
+class Gear
+  attr_reader :chainring, :cog
+  def initialize(chainring, cog)
+    @chainring = chainring
+    @cog = cog
   end
+
+  def gear_inches(diameter)
+    ratio * diameter
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+
 end
 
-module GearWrapper
-  def self.gear(args)
-    SomeFramework::Gear.new(
-      args[:chainring],
-      args[:cog],
-      args[:wheel])
-  end
-end
 
-puts GearWrapper.gear(
-  chainring: 52,
-  cog: 11,
-  wheel: Wheel.new(26, 1.5)).gear_inches
+puts Wheel.new(26, 1.5, 52, 11).gear_inches
--- wheel.rb
+++ wheel.rb
@@ -1,13 +1,17 @@
 class Wheel
-  attr_reader :rim, :tire
-
-  def initialize(rim, tire)
+  attr_reader :rim, :tire, :gear
+  def initialize(rim, tire, chainring, cog)
     @rim = rim
     @tire = tire
+    @gear = Gear.new(chainring, cog)
   end
 
   def diameter
     rim + (tire * 2)
+  end
+
+  def gear_inches
+    gear.gear_inches(diameter)
   end
 
   def circumference
